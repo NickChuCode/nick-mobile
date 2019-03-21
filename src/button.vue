a44c453563b3d56bb72ca1c58a687b4f882f2520
NickChuCode <nickchu15@outlook.com>
2019-03-22 01:28:37 +0800

    用 parcel 重构项目

---
+++ src/button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss">
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em; /* 很多人喜欢在按钮上加宽度width，其实没必要，只要左右各空出一个字符即可*/
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
