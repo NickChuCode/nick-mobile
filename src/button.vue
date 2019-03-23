4a21d0fc5f132e87c49e56fe9ed2412732eced4e
NickChuCode <nickchu15@outlook.com>
2019-03-24 00:49:01 +0800

    添加 icon 组件

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,6 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-        <svg v-if="icon" class="icon"><use :xlink:href="`#n-${icon}`"></use></svg>
+        <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
