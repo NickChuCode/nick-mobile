95a254b4f7ad68188e422825e437b0e788011350
NickChuCode <nickchu15@outlook.com>
2019-03-31 23:32:34 +0800

    用 row 和 col 组件取代 html + css 的粗糙写法，完成网格系统的雏形

--- src/col.vue
+++ src/col.vue
@@ -1,13 +1,32 @@
 <template>
-
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "col"
+        name: "col",
+        props: {
+            span: {
+                type: [String, Number] //vue的prop可以同时是多个类型，这样组件外部用span或者:span都可以
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    .col {
+        height: 100px;
+        background: grey;
+        border: 1px solid red;
+        width: 50%;
 
+        $class-prefix: col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n / 24) * 100%;
+            }
+        }
+    }
 </style>
