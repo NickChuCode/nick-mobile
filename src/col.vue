019422e3485146c471ba41b1f393333a9f1f8bff
NickChuCode <nickchu15@outlook.com>
2019-04-01 00:08:58 +0800

    给 col 组件添加 offset 功能

--- src/col.vue
+++ src/col.vue
@@ -1,15 +1,18 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
+    <div class="col" :class="[`col-${span}`, offset && `offset-${offset}`]">
         <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        name: "col",
+        name: "NickCol",
         props: {
             span: {
                 type: [String, Number] //vue的prop可以同时是多个类型，这样组件外部用span或者:span都可以
+            },
+            offset: {
+                type: [String, Number]
             }
         }
     }
@@ -26,6 +29,13 @@
         @for $n from 1 through 24 {
             &.#{$class-prefix}#{$n} {
                 width: ($n / 24) * 100%;
+            }
+        }
+
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n / 24) * 100%;
             }
         }
     }
