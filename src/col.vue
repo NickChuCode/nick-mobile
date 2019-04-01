257d82c41018707654aa0c4693987611c4c3cdac
NickChuCode <nickchu15@outlook.com>
2019-04-01 13:02:04 +0800

    重构 row 和 col 组件

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,6 @@
 <template>
-    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-        :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
-        <div style="border: 1px solid green; height: 100px;">
-            <slot></slot>
-        </div>
+    <div class="col" :class="colClass" :style="colStyle">
+        <slot></slot>
     </div>
 </template>
 
@@ -21,6 +18,22 @@
         data() {
             return {
                 gutter: 0
+            }
+        },
+        computed: {
+            // 计算属性应用的经典场景，如果一个属性需要根据另一个属性，这里是gutter的变化而变化，就需要使用计算属性
+            colStyle () {
+                return {
+                    paddingLeft: this.gutter/2+'px',
+                    paddingRight: this.gutter/2+'px'
+                }
+            },
+            colClass () {
+                let {span, offset} = this //注意这种写法
+                return [
+                    span && `col-${span}`,
+                    offset && `offset-${offset}`
+                ]
             }
         }
     }
