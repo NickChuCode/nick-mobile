40feb631f4b7aaa81a9e671916e5745906ecde27
NickChuCode <nickchu15@outlook.com>
2019-04-05 12:34:21 +0800

    重构了 col 组件

--- src/col.vue
+++ src/col.vue
@@ -46,6 +46,15 @@
                 gutter: 0
             }
         },
+        methods: {
+          createClasses (obj, str = '') {
+              if (!obj) {return []}
+              let array = []
+              if (obj.span) {array.push(`col-${str}${obj.span}`)}
+              if (obj.offset) {array.push(`offset-${str}${obj.offset}`)}
+              return array
+          }
+        },
         computed: {
             // 计算属性应用的经典场景，如果一个属性需要根据另一个属性，这里是gutter的变化而变化，就需要使用计算属性
             colStyle () {
@@ -58,12 +67,11 @@
                 let {span, offset, ipad, narrowPc, pc, widePc} = this //注意这种写法
                 console.log(ipad);
                 return [
-                    span && `col-${span}`,
-                    offset && `offset-${offset}`,
-                    ...(ipad ? [`col-ipad-${ipad.span}`] : []),
-                    ...(narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
-                    ...(pc ? [`col-pc-${pc.span}`] : []),
-                    ...(widePc ? [`col-wide-pc-${widePc.span}`] : []),
+                    ...this.createClasses({span, offset}),
+                    ...this.createClasses(ipad, 'ipad-'),
+                    ...this.createClasses(narrowPc, 'narrow-pc-'),
+                    ...this.createClasses(pc, 'pc-'),
+                    ...this.createClasses(widePc, 'wide-pc-'),
                 ]
             }
         }
