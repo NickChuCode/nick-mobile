5db181df280e4d1b79c9979142fc3bc9e7e0e36f
NickChuCode <nickchu15@outlook.com>
2019-04-06 00:14:40 +0800

    创建 layout content footer sider 组件

---
+++ src/layout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="layout">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "layout"
+    }
+</script>
+
+<style scoped lang="scss">
+    .layout {}
+</style>
