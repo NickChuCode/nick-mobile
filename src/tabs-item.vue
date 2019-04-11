e99580ae29399df36a831d306532b60c96bb3a09
NickChuCode <nickchu15@outlook.com>
2019-04-11 23:13:21 +0800

    tab 支持 disabled 功能

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tabs-item" @click="xxx" :class="classes">
+    <div class="tabs-item" @click="onClick" :class="classes">
         <slot></slot>
     </div>
 </template>
@@ -16,7 +16,8 @@
         computed: {
             classes () {
                 return {
-                    active: this.active
+                    active: this.active,
+                    disabled: this.disabled
                 }
             }
         },
@@ -42,7 +43,8 @@
             })
         },
         methods: {
-            xxx () {
+            onClick () {
+                if (this.disabled) {return}
                 this.eventBus.$emit('update:selected', this.name, this)
             }
         }
@@ -51,6 +53,7 @@
 
 <style scoped lang="scss">
     $blue: blue;
+    $disabled-text-color: grey;
     .tabs-item {
         flex-shrink: 0;
         padding: 0 2em;
@@ -61,6 +64,9 @@
         &.active {
             color: $blue;
             font-weight: bold;
+        }
+        &.disabled {
+            color: $disabled-text-color;
         }
     }
 </style>
