cdc9b5c15ce5a934cfca0cd01e765a52758b655b
NickChuCode <nickchu15@outlook.com>
2019-04-13 12:36:20 +0800

    解决当容器有 overflow hidden 造成的 bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="popover" @click.stop="xxx">
         <!--这里阻止冒泡，让我们点击弹出的popover的时候，不会消失-->
-        <div class="content-wrapper" v-if="visible" @click.stop>
+        <div class="content-wrapper" ref="contentWrapper" v-show="visible" @click.stop>
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 
@@ -19,14 +21,20 @@
         methods: {
             xxx () {
                 this.visible = !this.visible
-                console.log('切换 visible')
                 if (this.visible === true) {
-                    setTimeout(() => {
-                        console.log('新增 document click 监听器')
+                    this.$nextTick(() => {
+                        // 把 wrapper从div中移动出来，放在body下面（这样做是为了解决如果组件中加 overflow: hidden 而造成的 popover 显示不全的问题）
+                        // 而且这样移动，完全不会对元素造成任何的影响，绑定在上面的事件等属性照样可以正常的使用
+                        document.body.appendChild(this.$refs.contentWrapper)
+                        let { top, left } = this.$refs.triggerWrapper.getBoundingClientRect() // 这个得到的top left 是相对于浏览器的值
+                        console.log(top, left)
+                        this.$refs.contentWrapper.style.left = left + 'px'
+                        // 因为 contentWrapper 是相对于 body 定位，而triggerWrapper的值是相对于屏幕的值
+                        // 所以要加上 window.scrollY，同样window.scrollX也是同样的问题
+                        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
                         let eventHandler = () => {
-                            console.log('点击 body 就关闭 popover')
                             this.visible = false
-                            console.log('删除监听器')
                             document.removeEventListener('click', eventHandler) // 必须移除监听器，否则点击越多，积累的监听器越多
                         }
 
@@ -43,12 +51,11 @@
         display: inline-block;
         vertical-align: top;
         position: relative;
-        .content-wrapper {
-            position: absolute;
-            bottom: 100%;
-            left: 0;
-            border: 1px solid red;
-            box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-        }
+    }
+    .content-wrapper {
+        position: absolute;
+        border: 1px solid red;
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        transform: translateY(-100%);
     }
 </style>
